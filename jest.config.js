36c727951c4d15207baba2c1d79413887a0da8b2
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-22 20:28:24 +0100

    test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
-export default {
+export default{
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+  testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov']
 };
