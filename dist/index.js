64aa69b5928d37159859244fc350528519e22359
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-23 16:59:08 +0100

    fix: ts cors type def

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,5 @@
 import app from "./app";
 import config from './config/config';
-// app.listen(PORT, ()=>{ 
-//     console.log(`Server is running on port ${PORT}`)
-// })
 app.listen(config.PORT, () => {
     console.log(`
     Server running in ${config.NODE_ENV} mode
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import app from "./app"
 import config from './config/config';
 
-// app.listen(PORT, ()=>{ 
-//     console.log(`Server is running on port ${PORT}`)
-// })
-
 app.listen(config.PORT, () => {
   console.log(`
     Server running in ${config.NODE_ENV} mode
