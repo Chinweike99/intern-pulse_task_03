1b6affcde2dba60a9d0dfdfa3063f8e5e135b920
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-23 18:20:34 +0100

    fix: cors ts err

--- dist/routes/v1/index.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import routes from './paymentRoutes';
-const router = Router();
-router.use('/payments', routes);
-export default router;
--- dist/routes/v1/paymentRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { initatePaymentHandler, paymentStatusHandler } from "../../controllers/payment.controllers";
-const router = Router();
-router.post('/', initatePaymentHandler);
-router.get('/:reference', paymentStatusHandler);
-export default router;
