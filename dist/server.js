9ec3823d048588c62a9d8e86ebb485e362b2b470
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-23 15:32:57 +0100

    fix: server file

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,5 @@
 import app from "./app";
 import config from './config/config';
-const PORT = config.PORT || 3000;
 // app.listen(PORT, ()=>{ 
 //     console.log(`Server is running on port ${PORT}`)
 // })
